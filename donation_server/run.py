8135c63ccfe52d379b28b3cc13fec8c2b34c3cbf
Elliot <elliot.colquhoun@gmail.com>
2013-10-15 20:31:20 +1300

    Changed flask server file layout + content (simplified) and edited donation page (added donate button + background + layout)

--- donation_server/run.py
+++
@@ -1,3 +0,0 @@
-#!flask/bin/python
-from app import app
-app.run(debug = True, port = 8080, host='0.0.0.0')
--- donation_server/server.py
+++ donation_server/server.py
@@ -1,8 +1,13 @@
-from flask import render_template, request
-from app import app
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
     amount = request.args.get('amount')
     return render_template("mainpage.html", amount=amount)
+
+
+if __name__ == '__main__' :
+	app.run(debug = True, port = 8080, host='127.0.0.1')
