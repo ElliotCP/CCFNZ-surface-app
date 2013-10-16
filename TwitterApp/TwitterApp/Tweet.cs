f76e0fd1861ed035149b6195ced74f5afd4b86b6
npar224 <npar224@aucklanduni.ac.nz>
2013-10-16 21:31:06 +1300

    accessing old twitter api version

---
+++ TwitterApp/TwitterApp/Tweet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitterApp
+{
+    class Tweet
+    {
+        public string Title { get; set; }
+        public Uri Image { get; set; }
+        public Uri Link { get; set; }
+    }
+}
